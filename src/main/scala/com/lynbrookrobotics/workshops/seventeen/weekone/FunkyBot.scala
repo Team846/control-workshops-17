b25d52badfa47c01f19eb74de975dcfee58d75cf
Kunal Sheth <kunal@kunalsheth.info>
2017-10-06 04:22:04 -0700

    event

--- src/main/scala/com/lynbrookrobotics/workshops/seventeen/weekone/FunkyBot.scala
+++ src/main/scala/com/lynbrookrobotics/workshops/seventeen/weekone/FunkyBot.scala
@@ -1,12 +1,25 @@
 package com.lynbrookrobotics.workshops.seventeen.weekone
 
-import com.lynbrookrobotics.potassium.events.ImpulseEventSource
-import edu.wpi.first.wpilibj.RobotBase
+import com.lynbrookrobotics.potassium.streams._
+import com.lynbrookrobotics.potassium.events.{ContinuousEvent, ImpulseEventSource}
 import edu.wpi.first.wpilibj.hal.HAL
+import edu.wpi.first.wpilibj.{Compressor, Joystick, RobotBase, Solenoid}
 
 class FunkyBot extends RobotBase {
   override def startCompetition() = {
     val eventPollingSource = new ImpulseEventSource
+    val joystick = new Joystick(/*port =*/ 1)
+    val solenoid = new Solenoid(/*channel =*/ 2)
+    new Compressor().enabled()
+
+    // stream of `joystick.getButton` `kTrigger`
+    val triggerButtonStream: Stream[Boolean] = ???
+
+    // represent `triggerButtonStream` as an event
+    val triggerButtonEvent: ContinuousEvent = ???
+
+    // when `triggerButtonEvent` says that the trigger is pressed, solenoid.set(true)
+    // when `triggerButtonEvent` says that the trigger is released, solenoid.set(false)
 
     HAL.observeUserProgramStarting()
     while (true) {
