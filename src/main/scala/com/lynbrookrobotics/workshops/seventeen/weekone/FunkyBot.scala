ef3357bb1a576653a687db513c225f1341beaf16
Kunal Sheth <kunal@kunalsheth.info>
2017-10-06 04:45:22 -0700

    task

--- src/main/scala/com/lynbrookrobotics/workshops/seventeen/weekone/FunkyBot.scala
+++ src/main/scala/com/lynbrookrobotics/workshops/seventeen/weekone/FunkyBot.scala
@@ -1,14 +1,18 @@
 package com.lynbrookrobotics.workshops.seventeen.weekone
 
 import com.lynbrookrobotics.potassium.Signal
-import com.lynbrookrobotics.potassium.streams._
-import com.lynbrookrobotics.potassium.events.{ContinuousEvent, ImpulseEventSource}
+import com.lynbrookrobotics.potassium.clock.Clock
+import com.lynbrookrobotics.potassium.events.{ContinuousEvent, ImpulseEvent, ImpulseEventSource}
+import com.lynbrookrobotics.potassium.frc.WPIClock
+import com.lynbrookrobotics.potassium.tasks.ContinuousTask
 import edu.wpi.first.wpilibj.hal.HAL
 import edu.wpi.first.wpilibj.{Compressor, Joystick, RobotBase, Solenoid}
 
 class FunkyBot extends RobotBase {
   override def startCompetition() = {
     val eventPollingSource = new ImpulseEventSource
+    implicit val impulseEvent: ImpulseEvent = eventPollingSource.event
+    implicit val clock: Clock = WPIClock
     val joystick = new Joystick(/*port =*/ 1)
     val solenoid = new Solenoid(/*channel =*/ 2)
     new Compressor().enabled()
@@ -19,8 +23,12 @@ class FunkyBot extends RobotBase {
     // represent `triggerButtonStream` as an event
     val triggerButtonEvent: ContinuousEvent = ???
 
-    // when `triggerButtonEvent` says that the trigger is pressed, solenoid.set(true)
-    // when `triggerButtonEvent` says that the trigger is released, solenoid.set(false)
+    // first, `OpenValve` for 3 seconds
+    // then `WaitTask` for 3 seconds
+    // then `OpenValve` until trigger release
+    val flipFlop: ContinuousTask = ???
+
+    // on `triggerButtonEvent`, `flipFlop`
 
     HAL.observeUserProgramStarting()
     while (true) {
