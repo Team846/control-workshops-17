fca5efb4927fd749d0d5d434ecd534f51d20d172
Kunal Sheth <kunal@kunalsheth.info>
2017-10-06 04:45:22 -0700

    task

---
+++ src/main/scala/com/lynbrookrobotics/workshops/seventeen/weekone/OpenValve.scala
@@ -0,0 +1,8 @@
+package com.lynbrookrobotics.workshops.seventeen.weekone
+
+import com.lynbrookrobotics.potassium.tasks.ContinuousTask
+
+class OpenValve(/* what should this constructor take in? */) extends ContinuousTask {
+  override protected def onStart(): Unit = ???
+  override protected def onEnd(): Unit = ???
+}
